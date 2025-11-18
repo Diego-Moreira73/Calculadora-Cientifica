96fc5afb33279e474b742fbc0f966367f018aba5
Diego-Moreira73 <diego.andrade.moreira@gmail.com>
2025-11-18 18:30:04 -0300

    Update calculadora.php

--- calculadora.php
+++ calculadora.php
@@ -19,4 +19,53 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         $visor = $visor . $tecla;
     }
 }
+
 ?>
+
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <title>Calculadora Neon</title>
+    <link rel="stylesheet" href="calculadora.css">
+</head>
+<body>
+
+    <div class="calculadora">
+        
+        <h1>Calculadora</h1>
+
+        <form action="calculadora.php" method="post">
+            <input type="text" id="display" name="visor" value="<?php echo isset($visor) ? $visor : ''; ?>" readonly>
+
+            <div class="botao-grid">
+                
+                <button type="submit" name="tecla" value="Del" class="func">Del</button>
+                <button type="submit" name="tecla" value="(" class="func">(</button>
+                <button type="submit" name="tecla" value=")" class="func">)</button>
+                <button type="submit" name="tecla" value="/" class="func">/</button>
+
+                <button type="submit" name="tecla" value="7">7</button>
+                <button type="submit" name="tecla" value="8">8</button>
+                <button type="submit" name="tecla" value="9">9</button>
+                <button type="submit" name="tecla" value="*" class="func">*</button>
+
+                <button type="submit" name="tecla" value="4">4</button>
+                <button type="submit" name="tecla" value="5">5</button>
+                <button type="submit" name="tecla" value="6">6</button>
+                <button type="submit" name="tecla" value="-" class="func">-</button>
+
+                <button type="submit" name="tecla" value="1">1</button>
+                <button type="submit" name="tecla" value="2">2</button>
+                <button type="submit" name="tecla" value="3">3</button>
+                <button type="submit" name="tecla" value="+" class="func">+</button>
+
+                <button type="submit" name="tecla" value="0">0</button>
+                <button type="submit" name="tecla" value=".">.</button>
+                
+                <button type="submit" name="tecla" value="=" class="igual" style="grid-column: span 2;">=</button>
+            </div>
+        </form>
+    </div>
+
+</body>
+</html>
